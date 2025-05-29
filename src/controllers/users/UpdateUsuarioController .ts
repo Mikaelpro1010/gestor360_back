411189e3850854071836ee80fe9c14eceda2c11a
Mikaelpro1010 <mkpro10@alu.ufc.br>
2025-05-29 12:51:05 -0300

    Permitindo a edição de mais campos referente aos usuários cadastrados no sistema

--- src/controllers/users/UpdateUsuarioController .ts
+++ src/controllers/users/UpdateUsuarioController .ts
@@ -4,14 +4,62 @@ import { UpdateUsuarioService } from "../../service/users/UpdateUsuarioService";
 class UpdateUsuarioController {
   async handle(req: Request, res: Response) {
     const { id } = req.params;
-    const { name, email, cargoNaEmpresa } = req.body;
+    const {
+      name,
+      email,
+      password,
+      cpf,
+      cep,
+      estado,
+      cidade,
+      rua,
+      numero,
+      bairro,
+      complemento,
+      cnpj,
+      nomeFantasia,
+      razaoSocial,
+      registroMunicipal,
+      registroEstadual,
+      empresaCep,
+      empresaEstado,
+      empresaCidade,
+      empresaRua,
+      empresaNumero,
+      empresaBairro,
+      empresaComplemento,
+      cargoNaEmpresa,
+      nomeEmpresa,
+    } = req.body;
 
     try {
       const updatedUser = await UpdateUsuarioService({
         id,
         name,
         email,
+        password,
+        cpf,
+        cep,
+        estado,
+        cidade,
+        rua,
+        numero,
+        bairro,
+        complemento,
+        cnpj,
+        nomeFantasia,
+        razaoSocial,
+        registroMunicipal,
+        registroEstadual,
+        empresaCep,
+        empresaEstado,
+        empresaCidade,
+        empresaRua,
+        empresaNumero,
+        empresaBairro,
+        empresaComplemento,
         cargoNaEmpresa,
+        nomeEmpresa,
       });
 
       return res.status(200).json({
